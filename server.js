62df54e3003fd471ba31fd88e46850f015491384
jared-sukha <jared.sukha@gmail.com>
2022-08-17 12:28:42 +1200

    formatted quotes, added all quotes and bycharacter

--- server.js
+++ server.js
@@ -6,4 +6,14 @@ server.get('/quotes', (req, res) => {
   res.send(quotes[Math.floor(Math.random() * quotes.length)])
 })
 
+server.get('/quotes/all', (req, res) => {
+  res.send(quotes)
+})
+
+server.get('/quotes/bycharacter/:id', (req, res) => {
+  const charId = req.params.id.toLowerCase()
+  const charQuotes = quotes.filter((c) => c.character.toLowerCase() == charId)
+  res.send(charQuotes)
+})
+
 module.exports = server
